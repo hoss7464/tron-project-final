b774fe7cebe06c10aa20aa7c28fd73c203e393e0
hoss7464 <150793911+hoss7464@users.noreply.github.com>
2025-09-30 16:51:54 +0330

    desposit transaction

--- src/pages/investors/Buyers/Section1/Section1.tsx
+++ src/pages/investors/Buyers/Section1/Section1.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useCallback, useEffect } from "react";
 import "./Section1.css";
+import axios from "axios";
 import { useTronWallet } from "../../../../contexts/TronWalletContext";
 import { useFetchData } from "../../../../contexts/FetchDataContext";
 import {
@@ -48,22 +49,36 @@ import PopUp8 from "../../../../components/Popup/PopUp8";
 import { useDispatch } from "react-redux";
 import { showNotification } from "../../../../redux/actions/notifSlice";
 
+interface depositTrxData {
+  success: boolean;
+  depositId: string;
+}
+
+interface depositTrxConfirmation {
+  success: boolean;
+  data: {
+    txId: string;
+    depositId: string;
+  };
+}
+
 const Section1: React.FC = () => {
   const dispatch = useDispatch();
-  const { address, isConnectedTrading } = useTronWallet();
-  const { tradingAccountInfo, fetchData } = useFetchData();
+  const { address, isConnectedTrading, accessToken, transferTrx } =
+    useTronWallet();
+  const { tradingAccountInfo, resourceData } = useFetchData();
   const [deposit, setDeposit] = useState<string>("");
   const [depositError, setDepositError] = useState<string>("");
   const [changeApiOpen, setChangeApiOpen] = useState(false);
   const [apiKey, setApiKey] = useState("");
 
-
+  //to get data from server :
   useEffect(() => {
     if (tradingAccountInfo) {
       setApiKey(tradingAccountInfo.data.apiKey);
     }
   }, [tradingAccountInfo]);
-
+  //Function for deposit botton :
   const handleDepositChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
 
@@ -80,6 +95,125 @@ const Section1: React.FC = () => {
     }
   };
 
+  const handleDepositClick = async () => {
+    if (!address || isConnectedTrading === false) {
+      dispatch(
+        showNotification({
+          name: "deposit-error-5",
+          message: "Connect your wallet.",
+          severity: "error",
+        })
+      );
+      return;
+    }
+
+    const baseUrl = process.env.REACT_APP_BASE_URL;
+    const axiosTimeOut = Number(process.env.AXIOS_TIME_OUT);
+    const numericDepositAmount = Number(deposit);
+
+    try {
+      //payload for checking data towards the server :
+      const depositPayload = {
+        amount: numericDepositAmount,
+      };
+      //Send request to check the data towards the server
+      const depositResponse = await axios.post<depositTrxData>(
+        `${baseUrl}/Buyer/Deposit`,
+        depositPayload,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            accesstoken: accessToken,
+          },
+          timeout: axiosTimeOut,
+          validateStatus: (status: number) => status < 500,
+        }
+      );
+
+      if (depositResponse.data.success === true) {
+        //if depositResponse.data.success === true  ----> perform send TRX in tronLink
+
+        if (resourceData?.data.DappAddress === undefined) {
+          return;
+        }
+        //make transaction operation :
+        const depositTransferTrx = await transferTrx(
+          resourceData?.data.DappAddress,
+          numericDepositAmount
+        );
+        //if transaction in tronlink was successfull
+        if (depositTransferTrx.success === true) {
+          //send data towards the server :
+          const confirmDepostTrxPayload = {
+            txId: depositTransferTrx.txId,
+            depositId: depositResponse.data.depositId,
+          };
+          const confirmDepostTrx = await axios.post<depositTrxConfirmation>(
+            ``,
+            confirmDepostTrxPayload,
+            {
+              headers: {
+                "Content-Type": "application/json",
+                accesstoken: accessToken,
+              },
+              timeout: axiosTimeOut,
+              validateStatus: (status: number) => status < 500,
+            }
+          );
+
+          if (confirmDepostTrx.data.success === true) {
+            dispatch(
+              showNotification({
+                name: "deposit-success",
+                message: "Transaction Successful.",
+                severity: "success",
+              })
+            );
+            return;
+          } else {
+            dispatch(
+              showNotification({
+                name: "deposit-error-",
+                message: "Transaction failed",
+                severity: "error",
+              })
+            );
+            return;
+          }
+        } else {
+          dispatch(
+            showNotification({
+              name: "deposit-error-8",
+              message: "TronLink Error: transfer TRX faild",
+              severity: "error",
+            })
+          );
+          return;
+        }
+      } else {
+        //if depositResponse.data.success === false ----> make an error
+        dispatch(
+          showNotification({
+            name: "deposit-error-7",
+            message: "error in deposit data check",
+            severity: "error",
+          })
+        );
+        return;
+      }
+    } catch (error) {
+      dispatch(
+        showNotification({
+          name: "deposit-error-6",
+          message:
+            "When you depist once you must wait 2 minutes for next deposit operation",
+          severity: "error",
+        })
+      );
+      return;
+    }
+  };
+  //Function for showing the popup for API key:
   const handlePopUp8Click = () => {
     if (!address || isConnectedTrading === false) {
       dispatch(
@@ -93,11 +227,11 @@ const Section1: React.FC = () => {
     }
     setChangeApiOpen(true);
   };
-
+  //Function for close the popup for API key:
   const handlePopup8Close = useCallback(() => {
     setChangeApiOpen(false);
   }, []);
-
+  //Function to copy API key
   const handleCopy = () => {
     if (address) {
       navigator.clipboard.writeText(apiKey);
@@ -156,7 +290,7 @@ const Section1: React.FC = () => {
                           </FormAddInputWrapper2>
                         </FormAddInputIconWrapper>
                       </FormAddInputWrapper>
-                      <Sec1ButtonWrapper>
+                      <Sec1ButtonWrapper onClick={handleDepositClick}>
                         <Sec1ButtonText>Deposit</Sec1ButtonText>
                       </Sec1ButtonWrapper>
                     </Sec1CardThingsWrapper>
@@ -188,12 +322,13 @@ const Section1: React.FC = () => {
                         </SellersCardThingsNameWrapper>
                       </SellersCardThingsNameIconWrapper>
                       <SellersCardThingsNumberWrapper>
-                        {isConnectedTrading === true ? (<SellersCardThingsNumber>
-                          {(tradingAccountInfo?.data.buyerCredit)?.toLocaleString()}
-                        </SellersCardThingsNumber>) : (<SellersCardThingsNumber>
-                          _ _
-                        </SellersCardThingsNumber>)}
-                        
+                        {isConnectedTrading === true ? (
+                          <SellersCardThingsNumber>
+                            {tradingAccountInfo?.data.buyerCredit?.toLocaleString()}
+                          </SellersCardThingsNumber>
+                        ) : (
+                          <SellersCardThingsNumber>_ _</SellersCardThingsNumber>
+                        )}
                       </SellersCardThingsNumberWrapper>
                     </SellersCardThingsWrapper2>
                   </Sec1CardThingsWrapper>
